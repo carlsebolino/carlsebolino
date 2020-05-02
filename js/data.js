da9bc1841b0f469490e4eb552cd3cc6473f05930
carlSeb <carlsebolino@gmail.com>
2020-05-02 19:18:46 +0800

    initial commit

---
+++ js/data.js
@@ -0,0 +1,28 @@
+const main = {
+  name: 'Carl Sebolino',
+  mail: 'carlsebolino@gmail.com',
+  img: 'img/profile.jpg',
+  role: ['A Mediocre Software Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/carlseb'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/carlvtseb'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'carlsebolino@gmail.com'}`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
